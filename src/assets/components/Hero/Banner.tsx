a6e8bd1b8c6d639c9b2a3f57ddf257f33ac62fa0
gr8kev <82469036+gr8kev@users.noreply.github.com>
2025-04-09 19:10:13 +0100

    updated

--- src/assets/components/Hero/Banner.tsx
+++ src/assets/components/Hero/Banner.tsx
@@ -1,9 +1,7 @@
-import Image from "react-bootstrap/Image";
-
 export default function Banner() {
   return (
     <div className="container-fluid p-0 m-0">
-      <Image src="/public/hr.jpg" fluid className="w-100 h-100" />
+      <img src="/public/hr.jpg" alt="" className="w-100 h-100" />
     </div>
   );
 }
