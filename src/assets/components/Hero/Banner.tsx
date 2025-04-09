03bbecfeaff4259fb247d0d14139e7649dcd92c4
gr8kev <82469036+gr8kev@users.noreply.github.com>
2025-04-09 19:18:58 +0100

    image fixed

--- src/assets/components/Hero/Banner.tsx
+++ src/assets/components/Hero/Banner.tsx
@@ -1,7 +1,7 @@
 export default function Banner() {
   return (
     <div className="container-fluid p-0 m-0">
-      <img src="/public/hr.jpg" alt="" className="w-100 h-100" />
+      <img src="/hr.jpg" alt="" className="w-100 h-100" />
     </div>
   );
 }
